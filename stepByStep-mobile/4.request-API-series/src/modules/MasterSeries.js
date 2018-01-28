301546c1036a9edfc27060d34c66658e9fff970a
ivanrodricalleja <ivanrodricalleja@gmail.com>
2018-01-28 18:06:33 +0100

    Request API series

---
+++ stepByStep-mobile/4.request-API-series/src/modules/MasterSeries.js
@@ -0,0 +1,29 @@
+import React, { component, Component } from 'react';
+import { View, Text } from 'react-native';
+
+import axios from 'axios';
+
+class MasterSeries extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            series: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://seriesexample.azurewebsites.net/api/series')
+            .then(response => this.setState({ series: response.data }));
+    }
+
+    render() {
+        return(
+            <View>
+                <Text>Master Page</Text>
+            </View>
+        );
+    }
+}
+
+export { MasterSeries }
